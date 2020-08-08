59e782419a2c6c3ea6a3ea0798c7742832e8d073
mun-sathish <mun.sathish@gmail.com>
2020-08-08 19:45:12 +0530

    test

--- src/todo-app/src/containers/LeftContainer.js
+++ src/todo-app/src/containers/LeftContainer.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
-import { Input } from 'antd';
-import { ListPendingTodos } from '../components';
+import React, { Component } from "react";
+import { Input } from "antd";
+import { ListPendingTodos } from "../components";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Reducer1Actions, Reducer2Actions } from "../../../react-tutorial/redux-tut/store/actions";
+import {
+    Reducer1Actions,
+    Reducer2Actions,
+} from "../../../react-tutorial/redux-tut/store/actions";
 
 class LeftContainer extends Component {
     render() {
@@ -11,7 +14,7 @@ class LeftContainer extends Component {
         console.log(this.props.red1);
         return (
             <div>
-                <div>
+                {/* <div>
                     Type: {this.props.red1.type ? this.props.red1.type : "Yet to Set"}<br />
                     Data Available: {this.props.red1.dataAvailable ? "TRUE" : "FALSE"}<br />
                     Reducer2 Data: {this.props.red2.length > 0 ? this.props.red2 : "Empty Array"}<br /><br />
@@ -20,32 +23,43 @@ class LeftContainer extends Component {
                     <button onClick={() => this.props.chgR1DisableData()}>Data to FALSE</button>
                     <button onClick={() => this.props.chgR2Array(new Date().getTime())}>Add to Array</button>
                     <button onClick={() => this.props.chgR2Pop()}>Pop from Array</button>
-                </div>
-                <div style={{ padding: '20px' }}>
+                </div> */}
+                <div style={{ padding: "20px" }}>
                     <Input.Search
                         placeholder="Add Todo Item"
                         enterButton="ADD"
-                        onSearch={value => value && this.props.addTodo(value)}
+                        onSearch={(value) => value && this.props.addTodo(value)}
                     />
-                    <br /><br /><br />
-                    <ListPendingTodos todos={this.props.todos} completeTask={this.props.completeTask} />
+                    <br />
+                    <br />
+                    <br />
+                    <ListPendingTodos
+                        todos={this.props.todos}
+                        completeTask={this.props.completeTask}
+                    />
                 </div>
             </div>
         );
     }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     red1: state.reducer1,
-    red2: state.reducer2
+    red2: state.reducer2,
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
     chgR1Type: bindActionCreators(Reducer1Actions.chgR1Type, dispatch),
-    chgR1EnableData: bindActionCreators(Reducer1Actions.chgR1EnableData, dispatch),
-    chgR1DisableData: bindActionCreators(Reducer1Actions.chgR1DisableData, dispatch),
+    chgR1EnableData: bindActionCreators(
+        Reducer1Actions.chgR1EnableData,
+        dispatch
+    ),
+    chgR1DisableData: bindActionCreators(
+        Reducer1Actions.chgR1DisableData,
+        dispatch
+    ),
     chgR2Array: bindActionCreators(Reducer2Actions.chgR2Array, dispatch),
-    chgR2Pop: bindActionCreators(Reducer2Actions.chgR2Pop, dispatch)
-})
+    chgR2Pop: bindActionCreators(Reducer2Actions.chgR2Pop, dispatch),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(LeftContainer);
--- src/todo-app/src/containers/RightContainer.js
+++ src/todo-app/src/containers/RightContainer.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import { ListPendingTodos } from '../components';
+import React, { Component } from "react";
+import { ListPendingTodos } from "../components";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Reducer1Actions, Reducer2Actions } from "../../../react-tutorial/redux-tut/store/actions";
+import {
+    Reducer1Actions,
+    Reducer2Actions,
+} from "../../../react-tutorial/redux-tut/store/actions";
 
 class RightContainer extends Component {
     render() {
         return (
             <div>
-                <div>
+                {/* <div>
                     Type: {this.props.red1.type ? this.props.red1.type : "Yet to Set"}<br />
                     Data Available: {this.props.red1.dataAvailable ? "TRUE" : "FALSE"}<br />
                     Reducer2 Data: {this.props.red2.length > 0 ? this.props.red2 : "Empty Array"}<br /><br />
@@ -17,24 +20,30 @@ class RightContainer extends Component {
                     <button onClick={() => this.props.chgR1DisableData()}>Data to FALSE</button>
                     <button onClick={() => this.props.chgR2Array(new Date().getTime())}>Add to Array</button>
                     <button onClick={() => this.props.chgR2Pop()}>Pop from Array</button>
-                </div>
+                </div> */}
                 <ListPendingTodos todos={this.props.todos} isViewOnly />
             </div>
         );
     }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     red1: state.reducer1,
-    red2: state.reducer2
+    red2: state.reducer2,
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
     chgR1Type: bindActionCreators(Reducer1Actions.chgR1Type, dispatch),
-    chgR1EnableData: bindActionCreators(Reducer1Actions.chgR1EnableData, dispatch),
-    chgR1DisableData: bindActionCreators(Reducer1Actions.chgR1DisableData, dispatch),
+    chgR1EnableData: bindActionCreators(
+        Reducer1Actions.chgR1EnableData,
+        dispatch
+    ),
+    chgR1DisableData: bindActionCreators(
+        Reducer1Actions.chgR1DisableData,
+        dispatch
+    ),
     chgR2Array: bindActionCreators(Reducer2Actions.chgR2Array, dispatch),
-    chgR2Pop: bindActionCreators(Reducer2Actions.chgR2Pop, dispatch)
-})
+    chgR2Pop: bindActionCreators(Reducer2Actions.chgR2Pop, dispatch),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(RightContainer);
